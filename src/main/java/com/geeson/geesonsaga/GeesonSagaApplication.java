b69a12ede7da2a85970e945d4ec50185f23e608d
jay <jay@eximbay.com>
2025-06-25 16:30:21 +0900

    first commit

---
+++ src/main/java/com/geeson/geesonsaga/GeesonSagaApplication.java
@@ -0,0 +1,13 @@
+package com.geeson.geesonsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeesonSagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeesonSagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geeson/geesonsaga/GeesonSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeson.geesonsaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeesonSagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
